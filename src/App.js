0fd2359f8c2418e395540c0c6f1023c33b934175
samokello <samokello024@gmail.com>
2021-11-30 09:09:31 +0300

    created the login page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Home from './pages/Home';
 import './App.css';
 import ProductPage from './pages/ProductPage';
+import Login from './pages/Login';
+
 function App() {
   return (
-  <ProductPage />
+  <Login/>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,132 @@
+import React, {useState} from "react";
+import Announcement from "../components/Announcements/Announcement";
+import Nav from "../components/Navigation/Nav";
+import Footer from '../components/Footer/Footer';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import "./Login.css";
+
+const Login=()=>{
+
+    const[showPass , setShowPass]=useState(false);
+
+    const togglePassWardVisibility=()=>{
+setShowPass(!showPass)
+    }
+    return(
+<div>
+<Nav />
+<Announcement />
+
+<div className="login-container">
+
+
+<div className="login-content">
+<div className="enterprise-title">
+           <h1>Magoso Enterprices</h1>
+        </div>
+
+
+<form>
+<div>
+    <h1 className="enterprise-title">LOGIN</h1>
+   
+    <div className="form">
+    
+    <input type="text" placeholder=""  className="passward btn-red form__label"/>
+    <label for="email" className="label">Email</label>
+
+
+    </div>
+
+
+<div className="passward-holder ">
+<div className="form">
+    
+    <input type={showPass ? "text" :"password"} placeholder="" className="passward btn-red form__label"/>
+    <label for="email" className="label">password</label>
+</div>
+
+<div className="eye-icon" onClick={togglePassWardVisibility}>
+   {showPass ? <VisibilityIcon /> : <VisibilityOffIcon />}
+    </div>
+
+</div>
+
+<button className="login-btn">Login</button>
+
+
+<div className="checkbox-container">
+    <div className="check">
+    <div>
+<input type="checkbox"/>
+</div>
+
+<div>
+    <p>Remember me</p>
+</div>
+</div>
+
+<div className="link">
+  <p ><a href="#">Forgot your Passward</a> </p>
+</div>
+    </div>
+
+<div className="create-account">
+    <div>
+        <p>New to Magoso Enterprises</p>
+    </div>
+
+<div>
+   <button className="account">create Account</button>
+</div>
+
+
+</div>
+
+<div className="copyright">
+<p>Magoso Enterprices &copy;2021</p>
+</div>
+
+
+
+
+
+</div>
+
+</form>
+
+<div className="empty-div">
+
+
+</div>
+
+
+        </div>
+
+<div className="login-background">
+    <div className="heading">
+        <p className="text-cont">WELCOME TO</p>
+        <h1>Magoso Enterprices.</h1>
+<p className="text-pad">The home of Reliable Shopping</p>
+    </div>
+
+</div>
+
+
+
+
+
+
+        </div>
+
+        <Footer/>
+
+
+</div>
+
+        
+    )
+}
+
+export default Login
